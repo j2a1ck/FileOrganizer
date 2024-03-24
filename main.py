d8fb755279eec0a9a70ba7a1ed131ade48406a4f
jack <jack.70aw@gmail.com>
2024-03-24 15:30:16 +0330

    complete

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
-import os 
+import os
+from typee import file
 
+location = input("enter your directory")
+
+os.mkdir(location+"/Document")
+os.mkdir(location+"/Picture")
+os.mkdir(location+"/Video")
+os.mkdir(location+"/Compressed")
+os.mkdir(location+"/Audio")
+
+for i in os.listdir(location):
+    x = os.path.splitext(i)[1]
+
+    if x in file["Document"]:
+        os.rename(location+"/"+i, location+"/Document/"+i)
+    elif x in file["Picture"]:
+        os.rename(location+"/"+i, location+"/Picture/"+i)
+    elif x in file["Video"]:
+        os.rename(location+"/"+i, location+"/Video/"+i)
+    elif x in file["Compressed"]:
+        os.rename(location+"/"+i, location+"/Compressed/"+i)
+    elif x in file["Audio"]:
+        os.rename(location+"/"+i, location+"/Audio/"+i)
---
+++ typee.py
@@ -0,0 +1,29 @@
+
+file = {
+
+
+"Web": [".html5", ".html", ".htm", ".xhtml"], 
+	
+               "Picture": [".jpeg", ".jpg", ".tiff", ".gif",
+                           ".bmp", ".png", ".bpg", "svg",".heif", ".psd"], 
+	
+               "Video": [".avi", ".mkv",".flv", ".wmv",
+                         ".mov", ".mp4", ".webm", ".vob", 
+                         ".mng",".qt", ".mpg", ".mpeg", ".3gp"], 
+	
+              "Document": [".oxps", ".epub", ".pages", ".docx",
+                           ".txt", ".pdf", ".doc", ".fdf",
+                           ".ods",".odt", ".pwi", ".xsn",
+                           ".xps", ".dotx", ".docm", ".dox",
+                           ".rvg", ".rtf", ".rtfd", ".wpd", 
+                           ".xls", ".xlsx", ".ppt","pptx"], 
+	
+               "Compressed": [".a", ".ar", ".cpio", ".iso", 
+                              ".tar", ".gz", ".rz", ".7z",
+                              ".dmg", ".rar", ".xar", ".zip"], 
+	
+               "Audio": [".aac", ".aa", ".aac", ".dvf",
+                         ".m4a", ".m4b", ".m4p", ".mp3",
+                         ".msv", "ogg", "oga", ".raw", 
+                         ".vox", ".wav", ".wma"], 
+              } 
